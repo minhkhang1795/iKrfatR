78b9f8dba177d3eda4374095443c0ac96fed3f91
Khang <13646394+minhkhang1795@users.noreply.github.com>
2018-02-18 21:28:03 -0500

    dempth cam subscriber

--- depth_cam_subscriber.py
+++ depth_cam_subscriber.py
@@ -1,41 +1,55 @@
 import cv2
 import rospy
-from std_msgs.msg import UInt8MultiArray
+from cv_bridge import CvBridgeError, CvBridge
+from sensor_msgs.msg import Image, CompressedImage
 
 
 class DepthCamTest:
     def __init__(self):
+        self.bridge = CvBridge()
         rospy.init_node('my_node', anonymous=True)
-        rospy.Subscriber('/rbg_data', UInt8MultiArray, self.rgb_callback, queue_size=10)
-        rospy.Subscriber('/depth_data', UInt8MultiArray, self.depth_callback, queue_size=10)
+        rospy.Subscriber('/camera/color/image_raw', Image, self.rgb_callback, queue_size=10)
+        rospy.Subscriber('/camera/infra1/image_rect_raw', Image, self.depth_callback, queue_size=10)
         self.rgb_data = None
         self.depth_data = None
 
     def rgb_callback(self, data):
-        self.rgb_data = data
+        try:
+            self.rgb_data = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print e
 
     def depth_callback(self, data):
-        self.depth_data = data
+        # data.encoding = "bgr8"
+        try:
+            self.depth_data = self.bridge.imgmsg_to_cv2(data)
+        except CvBridgeError as e:
+            print e
 
     def show_rgb(self):
         while self.rgb_data is None:
             pass
+
+        r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            # print self.rgb_data.data
-            # cv2.imshow('', self.rgb_data.data.reshape(480, 640, 3))
+            r.sleep()
+            cv2.imshow('', self.rgb_data)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
     def show_depth(self):
         while self.depth_data is None:
             pass
+
+        r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            cv2.imshow('', self.depth_data.data.reshape(480, 640, 3))
+            r.sleep()
+            cv2.imshow('', self.depth_data)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
 
 if __name__ == '__main__':
     test = DepthCamTest()
-    test.show_rgb()
-    # test.show_depth()
+    # test.show_rgb()
+    test.show_depth()
